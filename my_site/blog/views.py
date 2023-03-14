f83c5c151a3370101c798b1695786f82582c3620
Mit Desai <118655128+mit8293@users.noreply.github.com>
2023-03-14 15:21:22 +0530

    day1

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 
 def post_detail(request):
